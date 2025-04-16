6849ff4097aeeaa56a916344491ea6ef840f4fbd
Vlad <vladmelnikov078@gmail.com>
2025-04-16 03:05:07 +0300

    Add slf4j-logback logging

--- src/main/java/org/example/dao/UserDaoImpl.java
+++ src/main/java/org/example/dao/UserDaoImpl.java
@@ -1,15 +1,18 @@
 package org.example.dao;
 
+
 import org.example.model.User;
 import org.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 public class UserDaoImpl implements UserDao {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
     @Override
     public Optional<User> findById(int id) {
         Transaction tran = null;
@@ -17,10 +20,12 @@ public class UserDaoImpl implements UserDao {
             tran = session.beginTransaction();
             User user = session.get(User.class, id);
             tran.commit();
+            logger.info("User found by Id: {}", id);
             return Optional.ofNullable(user);
         } catch (Exception e) {
             if (tran != null)
                 tran.rollback();
+            logger.error("Error finding user by Id: {}", id, e);
             throw new RuntimeException(e);
         }
     }
@@ -32,10 +37,12 @@ public class UserDaoImpl implements UserDao {
             tran = session.beginTransaction();
             List<User> users = session.createSelectionQuery("FROM User", User.class).getResultList();
             tran.commit();
+            logger.info("Users found");
             return users;
         } catch (Exception e) {
             if (tran != null)
                 tran.rollback();
+            logger.error("Error retrieving all users", e);
             throw new RuntimeException(e);
         }
     }
@@ -47,9 +54,11 @@ public class UserDaoImpl implements UserDao {
             tran = session.beginTransaction();
             session.persist(user);
             tran.commit();
+            logger.info("Users saved: {}", user);
         } catch (Exception e) {
             if (tran != null)
                 tran.rollback();
+            logger.error("Error saving user", e);
             throw new RuntimeException(e);
         }
     }
@@ -61,9 +70,11 @@ public class UserDaoImpl implements UserDao {
             tran = session.beginTransaction();
             session.merge(user);
             tran.commit();
+            logger.info("Users updated: {}", user);
         } catch (Exception e) {
             if (tran != null)
                 tran.rollback();
+            logger.error("Error updating user: {}", user.getId(), e);
             throw new RuntimeException(e);
         }
     }
@@ -76,9 +87,11 @@ public class UserDaoImpl implements UserDao {
             User user = session.get(User.class, id);
             session.remove(user);
             tran.commit();
+            logger.info("Users deleted: {}", user);
         } catch (Exception e) {
             if (tran != null)
                 tran.rollback();
+            logger.error("Error deleting user: {}", id, e);
             throw new RuntimeException(e);
         }
     }
